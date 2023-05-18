6c35ff113fb22dd95b2c759338bbebe3e92f3ffe
Enzo He <111211290+Enzo-He@users.noreply.github.com>
2023-05-18 15:01:13 +0800

    .

--- frontend/src/pages/components/LoginPageComponent.js
+++ frontend/src/pages/components/LoginPageComponent.js
@@ -1,6 +1,6 @@
 import { Container, Row, Col, Form, Button, Alert } from "react-bootstrap";
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+// import { Link, useNavigate } from "react-router-dom";
 import Spinner from "react-bootstrap/Spinner";
 
 const LoginPageComponent = ({
@@ -18,7 +18,7 @@ const LoginPageComponent = ({
   const [errorMessage, setErrorMessage] = useState("");
   const [ipAddress, setIpAddress] = useState("");
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
