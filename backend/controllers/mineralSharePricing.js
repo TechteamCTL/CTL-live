c74abddd12ed6d9df5671c1314ced1b803e3d73f
Enzo He <111211290+Enzo-He@users.noreply.github.com>
2023-07-14 08:38:05 +0800

    Update mineralSharePricing.js

--- backend/controllers/mineralSharePricing.js
+++ backend/controllers/mineralSharePricing.js
@@ -57,7 +57,8 @@ const adminCreatePrice = async (req, res, next) => {
   }
 };
 
-const adminUpdateMineralsPrice = async (req, res, next) => {
+
+const updateMineralPrices = async () => {
   try {
     const response = await axios.get('https://metals-api.com/api/latest?access_key=8k681h1l6p7sh81nsl8ja22dj7914d7665putqbc7dnyupec7e1420no7w3q&base=AUD&symbols=XAU%2CXAG%2CXPT%2CXPD%2CIRON%2CALU%2CLCO%2CXCU%2CLEAD%2CNI%2CTIN%2CZNC');
     const data = response.data.rates;
@@ -83,19 +84,35 @@ const adminUpdateMineralsPrice = async (req, res, next) => {
       )
       .catch(err => console.error(`Failed to find and update document with symbol ${symbol}: ${err}`));
     }
+    console.log("Minerals Price Has Been Updated");
+  } catch(err) {
+    console.error(err);
+  }
+}
 
+const adminUpdateMineralsPrice = async (req, res, next) => {
+  try {
+    await updateMineralPrices();
     res.status(200).send("Prices and dates updated successfully");
   } catch(err) {
     next(err);
   }
 }
 
-cron.schedule('0 05 16 * * *', adminUpdateMineralsPrice, {
+cron.schedule('0 42 12 * * *', updateMineralPrices, {
   scheduled: true,
   timezone: "UTC"
 });
 
-
 module.exports = { getMineralPrice, adminCreatePrice, adminUpdateMineralsPrice }
+
+/* 
+api.openweathermap.org/data/2.5/forecast/daily?lat={维度}&lon={经度}&cnt={天}&appid=2661e8453a0019c38576fec6d7649d24
+*/
+
+// weather api
+// http://api.weatherapi.com/v1/forecast.json?key=3e572308a2a646d0b5c82751231307&q=-31.106598, 122.031713&days=4
+
+
 
 
